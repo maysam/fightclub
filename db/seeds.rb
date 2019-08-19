5af333782267f1c8d37338f4e243c6950cad0119
maysam torabi <kherad@gmail.com>
2019-08-19 16:31:51 +0430

    L'arène n'est pas vide au premier démarrage de l'appli.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Character.create name: 'Greymon', life_point: 5, attack_point: 6, defence_point: 6
+Character.create name: 'Agumon', life_point: 6, attack_point: 4, defence_point: 5
+Character.create name: 'Tailmon', life_point: 7, attack_point: 3, defence_point: 5
+Character.create name: 'Patamon', life_point: 8, attack_point: 2, defence_point: 4
+Character.create name: 'Palmon', life_point: 9, attack_point: 1, defence_point: 3
